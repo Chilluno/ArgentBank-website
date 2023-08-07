ca51011b07249beb92d3579d148e8ef0e50abc25
Wesley Slimani <wesley.slimani@gmail.com>
2023-08-07 08:35:45 +0200

    feat: added username edit form

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { Account } from "../components/Account"
 
 let accountDetails = null;
@@ -20,6 +20,13 @@ const placeholderDetails = [{
 }]
 
 export const Profile = () =>{
+	const [showForm, setShowForm] = useState(false);
+	const [username, setUsername] = useState("");
+
+	const showUserForm = () => {
+		setShowForm(!showForm);
+	}
+
 	if(localStorage.getItem("user")){
 		accountDetails = 	JSON.parse(localStorage.getItem("user"));
 		console.log(accountDetails);
@@ -35,7 +42,49 @@ export const Profile = () =>{
 		<br></br>
 		{`${accountDetails.firstName} ${accountDetails.lastName} !`}
 		</h1>
-	<button className="edit-button">Edit Name</button>
+{
+	(showForm === false && (<button className="edit-button" onClick={showUserForm}>Edit Name</button>))
+}
+{
+	(showForm === true && (<section className="edit-content"><h1>Edit User Info</h1>
+	<form>
+	<div className="input-wrapper">
+	<label htmlFor="username">Username</label>
+	<input
+	type="text"
+	id="username"
+	value={username}
+	onChange={(e) => setUsername(e.target.value)}
+	/>
+	</div>
+	<div className="input-wrapper">
+	<label htmlFor="firstname">First Name</label>
+	<input
+	type="text"
+	id="firstname"
+	value={accountDetails.firstName}
+	disabled
+	/>
+	</div>
+	<div className="input-wrapper">
+	<label htmlFor="lastname">Last Name</label>
+	<input
+	type="text"
+	id="lastname"
+	value={accountDetails.lastName}
+	disabled
+	/>
+	</div>
+	<div>
+	<button type="submit" className="save-button">
+		Save
+	</button>
+	<button type="button" className="cancel-button" onClick={showUserForm}>
+		Cancel
+	</button>
+	</div>
+	</form></section>))
+}
 	</div>
 	<h2 className="sr-only">Accounts</h2>
 	{
